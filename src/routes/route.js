044dc5700224113d2fa2669eef19a3f2ab6f6dfd
tilak088 <tilakchandele431@gmail.com>
2022-02-23 10:23:57 -0700

    22assign

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,12 +1,32 @@
-const module2 = require('/util/helper.js')
 const express = require('express');
-
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
-    res.send('My first ever api!')
-    console.log (module2.Date)
+router.get('/students/:name', function(req, res) {
+    let studentName = req.params.name
+    console.log(studentName)
+    res.send(studentName)
+})
+
+// 1. API will return list of movies in Array
+router.get('/movies', function(req, res){
+    res.send('["Lucifer", "Pirates of the carribien", "Little things", "Mirzapur", "Money Heist"]')
 });
 
+// 2. API will return movei by index in Array
+router.get('/movies/:movieID', function(req, res){
+    mov = ["Lucifer", "Pirates of the carribien", "Little things", "Mirzapur", "Money Heist"]
+    let value = req.params.movieID
+    if(value>mov.length-1){
+        res.send("movie not found")
+    } else {
+        res.send(mov[value])
+    }
+});
+
+// 4. Return an Array
+router.get('/movie', function(req, res){
+    res.send([ {id:1,name: 'The Shining'}, {id:2,name: 'Incendies'}, {id:3,name: 'Rang de Basanti'}, {id:4,name: 'Finding Demo'} ])
+})
+
+
 module.exports = router;
-// adding this comment for no reason
